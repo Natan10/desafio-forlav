8100d055f5e71d1dd79bf64be45b767cc3a80c54
Natan <natan-moreira1@hotmail.com>
2021-07-07 23:16:14 -0300

    feat: config devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_manager!
 
+  layout :layout_by_resource
+
+  def current_user
+    @current_user ||= current_manager 
+  end
+
+  private
+
+  def layout_by_resource
+    if current_manager
+      'application'
+    else
+      'auth'
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+  def index
+    # TODO: index users
+  end
 end
