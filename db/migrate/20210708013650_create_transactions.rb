fbaef46a673a290f3e63b1c5d7cdcb5ecb6bc07b
Natan <natan-moreira1@hotmail.com>
2021-07-07 22:38:26 -0300

    feat: create transaction model

---
+++ db/migrate/20210708013650_create_transactions.rb
@@ -0,0 +1,11 @@
+class CreateTransactions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :transactions do |t|
+      t.decimal :value, precision: 8, scale: 2, null: false 
+      t.string :status, default: 0, null: false 
+      t.references :wallet, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_013429) do
+ActiveRecord::Schema.define(version: 2021_07_08_013650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2021_07_08_013429) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_managers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_managers_on_reset_password_token", unique: true
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.decimal "value", precision: 8, scale: 2, null: false
+    t.string "status", default: "0", null: false
+    t.bigint "wallet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["wallet_id"], name: "index_transactions_on_wallet_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,5 +52,6 @@ ActiveRecord::Schema.define(version: 2021_07_08_013429) do
     t.index ["user_id"], name: "index_wallets_on_user_id"
   end
 
+  add_foreign_key "transactions", "wallets"
   add_foreign_key "wallets", "users"
 end
