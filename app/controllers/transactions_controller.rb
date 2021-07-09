2f9b091ac6d03e77a92aa3c5a8aae6e59dc0d6e0
Natan <natan-moreira1@hotmail.com>
2021-07-08 22:38:30 -0300

    refactor: refactor controller transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,27 +7,31 @@ class TransactionsController < ApplicationController
     if @transaction.value.nil?
       flash[:error] = "Valor não pode ficar em branco"  
     else
-      if @transaction.credit?
-        @wallet.balance += @transaction.value 
-        @wallet.save 
-        @transaction.save 
-        flash[:success]= "Crédito feito com sucesso!"
-      else
-        if @transaction.value <= @wallet.balance
-          return if @transaction.value.nil?
-          @wallet.balance -= @transaction.value
-          @wallet.save
-          @transaction.save
-          flash[:success]= "Débito feito com sucesso!"
-        elsif @transaction.value > @wallet.balance
-          flash[:error]= "Saldo insuficiente!!!"
-        end
-      end
+      @transaction.credit? ? credit : debit 
     end  
     redirect_to user_path(@wallet.user) 
   end
 
   private
+
+  def credit 
+    @wallet.balance += @transaction.value 
+    @wallet.save 
+    @transaction.save 
+
+    flash[:success]= "Crédito feito com sucesso!"
+  end
+
+  def debit
+    if @transaction.value <= @wallet.balance
+      @wallet.balance -= @transaction.value
+      @wallet.save
+      @transaction.save
+      flash[:success]= "Débito feito com sucesso!"
+    elsif @transaction.value > @wallet.balance
+      flash[:error]= "Saldo insuficiente!!!"
+    end
+  end
 
   def transaction_params
     params.require(:transaction).permit(:wallet_id, :value,:status)
